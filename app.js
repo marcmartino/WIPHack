75b15f44bcbace6cbd6ba9bc1b495fef49b9f3c2
markubulap <marklap@markubulap.(none)>
2013-02-23 16:43:03 -0800

    chat server starting to work

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
 var io = require('socket.io').listen(8080);
 
 io.sockets.on('connection', function (socket) {
-  socket.on('message', function (msg) {
-  	io.emit("message", "poop");
 
-  	console.log("yay i got a message!");
-  	console.log("it reads: " + msg);
+  socket.on('message', function (msg) {
+  	//socket.emit("message", "poop");
+  	var msgFromClient = parseSocketMessage(msg);
+  	console.log(msgFromClient);
+  	ioActions[msgFromClient.action](socket, msgFromClient);
   });
   socket.on('disconnect', function () { });
 });
+
+var parseSocketMessage = function (msg) {
+	var decoded = JSON.parse(msg);
+	return decoded;
+}
+var encodeSocketMessage = function (action, msg) {
+	return JSON.stringify(
+		{action: action, msgContents: JSON.stringify(msg)});
+}
+
+var sendAllMessages = function (sock) {
+	var allMessages
+};
+
+var ioActions = {
+	viewAll: function (socket, msg) {
+		/*{
+			eventID: 1
+		}*/
+		socket.emit(encodeSocketMessage("viewAll"),
+			{socketInfo: "stuff"});
+	},
+	"new": function (socket, msg) {
+		/*{
+			timestamp: 1231241241,
+			location: {lat: 12, lang: 13},
+			eventID: 1,
+			msgContents: "poops"
+		}*/
+	},
+	update: function (socket, msg) {
+		/*{
+			postID: 12,
+			timestamp: 123123124,
+			msgContents: "fawefawe"
+		}*/
+	},
+	delete: function (socket, msg) {
+		/*{
+			postID: 12,
+			timestamp: 123124124,
+		}*/
+	}
+}
